d5381ce0dfebf950a3c3a8c08be05d6871e0a218
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-04-28 16:56:36 -0600

    se agregaron comentarios

--- hello-world/src/guessGame.rs
+++ hello-world/src/guessGame.rs
@@ -1,12 +1,12 @@
-use std::io;
+use std::io; //import del parquete input output
 
-fn main(){
+fn main(){ //punto de entrada del programa main
 
     println!("Guess the number!");
 
     println!("Please input your guess.");
 
-    let mut guess = String::new();
+    let mut guess = String::new(); //mut hace referencia a que una variable puede cambiar de valor
 
     io::stdin()
         .read_line(&mut guess)
